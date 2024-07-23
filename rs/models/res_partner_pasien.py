929fb1441c86db2aa6b2527bd370fa7892f54109
bintangpratamaa <bintangpratamayudhistira@mail.ugm.ac.id>
2024-07-23 18:14:50 +0700

    update field res_partner

--- rs/models/res_partner_pasien.py
+++ rs/models/res_partner_pasien.py
@@ -14,7 +14,16 @@ class ResPartnerPasien(models.Model):
     rs_goldar_id = fields.Many2one("rs_goldar")
     rs_stat_kawin_id = fields.Many2one("rs_stat_kawin")
     rs_gender_id = fields.Many2one("rs_gender")
-
+    address = fields.Char(string="alamat")
+    no_kk = fields.Integer(string="no kk", help="masukan nomor kartu keluarga")
+    no_nik = fields.Integer(string="no nik", help="masukan nomor induk kependudukan")
+    no_kis = fields.Integer(string="no kis", help="masukan nomor Kartu Indonesia Sehat")
+    no_hp = fields.Char(string="no hp", help="masukan no hp",default='08')
+    tempat_lahir = fields.Char(string="Tempat Lahir")
+    pendidikan = fields.Selection([("sd/mi","SD/MI"),("smp/mts","SMP/MTS"),("sma/k/ma","SMA/K/MA")], string="pendidikan",help="Pendidikan Terakhir")
+    pekerjaan = fields.Char(string="Pekerjaan")
+    suku = fields.Char(string="Nama Suku",help="contoh suku jawa")
+    zip_code = fields.Integer(string="kode pos")
     @api.model
     def create(self, vals):
         if self.env.context.get('force_flag_pasien_true', False):
